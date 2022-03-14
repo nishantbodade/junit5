56d89ef28edb732db0c2ca6dec07d7d4aaf33978
Nishant Bodade <nishantbodade@gmail.com>
2022-03-14 11:06:52 +0530

    refactorinh the code--clean code

--- JunitCource/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ JunitCource/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -6,13 +6,23 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	StringHelper helper=new StringHelper();
+	
 	@Test
-	public void test() {
+	public void testTruncateAInFirst2Positions_AinFirstTwoPossition() {
 		//AACD => CD 	CDAA=>CDAA  ACDA=>CDA
-		StringHelper helper=new StringHelper();
+	
 		//expectec,actual
 		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+		
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions2() {
+		
+		//expectec,actual
 		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+		
 	}
 
 }
