621ebd88cab75db1388d1e91112324b1db1cce60
Nishant Bodade <nishantbodade@gmail.com>
2022-03-22 09:40:02 +0530

    Arguments Capture demo for one and more than one methos calls

--- JunitCource/src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
+++ JunitCource/src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.in28minutes.data.api.TodoService;
 import com.in28minutes.data.stub.TodoServiceStub;
@@ -83,6 +85,44 @@ public class TodoBusinessImplMockTest {
 		 verify(todoServiceMock,times(1)).deleteToDo("Learn to Dance");//how many time to call ,in this one time only
 		verify(todoServiceMock).deleteToDo("Learn to Dance");//method call check ,on Learn to Dance string value pass
 		verify(todoServiceMock,never()).deleteToDo("todoServiceMock");//never call
+		//then(todoServiceMock).should().deleteToDo("Learn to Dance");// then should nevel call or call
+		
+		
+		
+	}
+
+	@Test
+	public void testBDDArgumentCapture() {
+		
+		//declare args
+		ArgumentCaptor<String> argumentCaptor=ArgumentCaptor.forClass(String.class);
+		
+		//define args capture on specific methods call
+		//capture the args
+		
+		//given
+		TodoService todoServiceMock =mock(TodoService.class);
+		
+		List<String> todo=Arrays.asList("Learn Spring MVC", "Learn Spring",
+					"Learn to Dance");
+		
+		given(todoServiceMock.retrieveTodos("dummy")).willReturn(todo);//syntax change for BDD
+		
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		//when
+		 todoBusinessImpl
+				.deleteTodosRelatedToSpring("dummy");
+		
+		//then
+
+		then(todoServiceMock).should().deleteToDo(argumentCaptor.capture());// then should nevel call or call
+		//then(todoServiceMock).should(times(2)).deleteToDo(argumentCaptor.capture());// thwo times or more times method calls then do this
+		assertThat(argumentCaptor.getValue(), is("Learn to Dance"));
+		
+		
+		
+		//assertThat(argumentCaptor.getAllValues().size(), is(2));//check the size
 		
 		
 		
